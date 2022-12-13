a5a75c9f2ebcaa76d1cf015c245c64a9cfccb4b6
awilkes2020 <awilkes2020@yahoo.com>
2022-12-12 20:39:52 -0500

    xyz

--- src/Components/VideoComponent.js
+++ src/Components/VideoComponent.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 
 export function PhilVideoPlayer(props){
-    return <ReactPlayer id="videoPlayer" 
-    url={props.url} 
-    ref={props.playerRef} 
-    onProgress={props.onProgress}
-    controls={true}
-    playing={props.playing}
-    onPlay={props.play}
-    onPause={props.pause}
-    />
+    return ( 
+        <ReactPlayer id="videoPlayer" 
+            url={props.url} 
+            ref={props.playerRef} 
+            onProgress={props.onProgress}
+            controls={true}
+            playing={props.playing}
+            onPlay={props.play}
+            onPause={props.pause}
+        />
+    )
 }
