7dea16c7f7e3bf8d165257ef44c48a1a4c4b44f9
Nicholas Johnson <johnson.8777@buckeyemail.osu.edu>
2022-12-12 22:15:33 -0500

    change

--- src/Components/VideoComponent.js
+++ src/Components/VideoComponent.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import ReactPlayer from 'react-player'
+import ReactPlayer from './react-player'
 
 export function PhilVideoPlayer(props) {
     const playerRef = useRef(null);
